0a585cf27681bbb0eb39c36b53a5fcc3ca0fd6c3
Vanfarock <vinicius.m.m.2002@gmail.com>
2022-01-27 17:38:01 -0300

    feat: add image wrapper

---
+++ src/custom_image/custom_image.rs
@@ -0,0 +1,29 @@
+use image::io::Reader as ImageReader;
+use image::DynamicImage;
+
+pub trait ImageFilter {}
+
+pub struct CustomImage {
+    pub internal_image: DynamicImage,
+    pub path: String,
+    pub filename: String,
+    pub full_path: String,
+}
+
+impl CustomImage {
+    pub fn new(path: String, filename: String) -> Self {
+        let full_path = format!(
+            "{}{}",
+            path.trim_end_matches("/"),
+            filename.trim_start_matches("/")
+        );
+        let internal_image = ImageReader::open(full_path).unwrap().decode().unwrap();
+
+        CustomImage {
+            internal_image,
+            path,
+            filename,
+            full_path,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use std::{env, error::Error};
 
-use image::{io::Reader as ImageReader};
+use image::io::Reader as ImageReader;
 
+use crate::custom_image::custom_image::CustomImage;
+
+mod custom_image;
 mod filters;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut current_folder = env::current_dir()?;
     current_folder.push("src\\assets\\");
 
-    let path = current_folder.to_str().unwrap();
-    let img = ImageReader::open(path.to_string() + "image1.jpg")?.decode()?;
+    let path = current_folder.to_str().unwrap().to_string();
+    let filename = "image1.jpg".to_string();
 
-    filters::average_blur::run(&mut img.clone(), path, 15).expect("");
-    
+    let custom_image = CustomImage::new(path, filename);
+
     println!("Finished!");
 
     Ok(())
